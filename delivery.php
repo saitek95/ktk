9afd48ac691696ba52b273489d0537e34d93cdb6
i.notfullin <saitek95@mail.ru>
2025-02-26 16:07:52 +0300

    start

---
+++ delivery.php
@@ -0,0 +1,71 @@
+<?php include 'header.php';?>
+<div class="container">
+    <div class="breadcrumb">
+        <ul>
+            <li><a href="">Главная</a></li>
+            <li>/</li>
+            <li>ДОСТАВКА</li>
+        </ul>
+    </div>
+    <h1>ДОСТАВКА</h1>
+    <div class="main_delivery">
+        <div class="top_block">
+            <div class="item">
+                <h4>САМОВЫВОЗ</h4>
+            </div>
+            <div class="item">
+                <p>Уважаемые покупатели, обращаем Ваше внимание: свой заказ Вы можете забрать в любой рабочий день недели
+                    с 09:00 до 18:00 сразу после подтверждения наличия заказанного товара менеджером нашего
+                    интернет-магазина!
+                </p>
+                <p>г. Москва, ул. Иркутская, дом № 17, строение 14, склад 3, ворота 6</p>
+                <a href="">Смотреть на карте</a>
+            </div>
+        </div>
+        <div class="grid_items">
+            <div class="item">
+                <p class="name">МОСКВА</p>
+                <p class="txt">При заказе на сумму</p>
+                <p class="blue_txt">
+                    от 20 000 рублей
+                    <span>доставка бесплатно.</span>
+                </p>
+                <p class="txt">
+                    Доставка по Москве производится до подъезда, подъем на этаж не осуществляется.
+                </p>
+                <p class="txt">
+                    Доставка осуществляется в рабочие дни Пн-Пт с 9-00 до 18-00
+                </p>
+                <p class="txt">
+                    Доставка осуществляется в течение 2-3 рабочих дней. При отсутствии корректировки заказа по Вашей инициативе.
+                </p>
+            </div>
+            <div class="item">
+                <p class="name">россия</p>
+                <p class="txt">При заказе на сумму</p>
+                <p class="blue_txt">
+                    от 20 000 рублей
+                    <span>доставка бесплатно.</span>
+                </p>
+                <p class="txt">
+                    Доставка по России производится до указанного вами терминала транспортной компании.
+                </p>
+                <p class="txt">
+                    Отправка осуществляется в рабочие дни с Пн-Пт с 9-00 до 18-00
+                </p>
+                <p class="txt">
+                    Транспортные компании:
+                </p>
+                <div class="brands_list">
+                    <img src="./assets/img/brands_items_1.png" alt="">
+                    <img src="./assets/img/brands_items_2.png" alt="">
+                    <img src="./assets/img/brands_items_3.png" alt="">
+                    <img src="./assets/img/brands_items_4.png" alt="">
+                    <img src="./assets/img/brands_items_5.png" alt="">
+                    <img src="./assets/img/brands_items_6.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include 'footer.php'; ?>

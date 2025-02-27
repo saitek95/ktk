85e9910cdc90a0444e816daa6a7adff4797492d5
i.notfullin <saitek95@mail.ru>
2025-02-27 15:47:59 +0300

    start

---
+++ color-map.php
@@ -0,0 +1,56 @@
+<?php include 'header.php';?>
+<div class="container">
+    <div class="breadcrumb">
+        <ul>
+            <li><a href="">Главная</a></li>
+            <li>/</li>
+            <li>кАРТА ЦВЕТОВ</li>
+        </ul>
+    </div>
+    <h1>кАРТА ЦВЕТОВ</h1>
+    <div class="main_color-map">
+        <div class="grid_items">
+            <a href="" class="item">
+                <p class="title">
+                    КАРТА ЦВЕТОВ НИТОК
+                </p>
+                <div class="bottom_block">
+                    <p class="download">
+                        <span>Скачать</span>
+                        <img src="./assets/svg/DownloadSimple.svg" alt="" class="svg">
+                    </p>
+                    <p class="btn_blue">.pdf</p>
+                </div>
+            </a>
+            <a href="" class="item">
+                <p class="title">
+                    КАРТА ЦВЕТОВ НИТОК
+                </p>
+                <div class="bottom_block">
+                    <p class="download">
+                        <span>Скачать</span>
+                        <img src="./assets/svg/DownloadSimple.svg" alt="" class="svg">
+                    </p>
+                    <p class="btn_blue">.pdf</p>
+                </div>
+            </a>
+            <a href="" class="item">
+                <p class="title">
+                    КАРТА ЦВЕТОВ НИТОК
+                </p>
+                <div class="bottom_block">
+                    <p class="download">
+                        <span>Скачать</span>
+                        <img src="./assets/svg/DownloadSimple.svg" alt="" class="svg">
+                    </p>
+                    <p class="btn_blue">.pdf</p>
+                </div>
+            </a>
+            <div class="bottom_item">
+                <p>НАЙДЕМ ДЛЯ ВАС <span>ЛЮБОЙ ТОВАР</span></p>
+                <a href="" class="btn_blue">Перейти</a>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include 'footer.php'; ?>
